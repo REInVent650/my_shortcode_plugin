8cb8cb5f15914c49184e0dfb91e0ae674dcc88b7
REInVent650 <78056322+REInVent650@users.noreply.github.com>
2021-01-29 15:12:58 +1300

    Update my-shortcode-plugin.php
    Preparation for supporting enclosing shortcodes 
    Migrated from htmlspecialchars() to wp_kses()

--- my-shortcode-plugin.php
+++ my-shortcode-plugin.php
@@ -1,18 +1,29 @@
 <?php
 /**
  * Plugin Name: My Shortcode Plugin
- * Description: To display a custom message do: 
+ * Description: To display a custom message do: [my_shortcode_plugin name="name here"]
  * Version: 0.0.1
  * Author: REInVent
  */
 
- function my_shortcode( $atts = array() ) {
+ function my_shortcode( $atts = array(), $content = null ) {
+   
+  //array of allowed html for tags wp_kses
+  $allow = array(
+    'a' => array(
+        'href' => array(),
+        'title' => array()
+    ),
+    'br' => array(),
+    'em' => array(),
+    'strong' => array(),
+);
 
    //check if name has been set otherwise set the value to world
    $atts_array = shortcode_atts( array( 'name' => 'World' ), $atts );
    
    //create a custom string to greet the user - don't trust the user
-   $text = "Hello " .htmlspecialchars($atts_array['name'])."!"."<br>";
+   $text = "Hello " .wp_kses($atts_array['name'], $allow)."!"."<br>";
    
    //return our custom string
    return $text;
