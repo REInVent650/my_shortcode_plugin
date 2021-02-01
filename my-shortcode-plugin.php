76445d3f8027a1e81b0066ecf1fb759fe264eae9
REInVent650 <78056322+REInVent650@users.noreply.github.com>
2021-02-01 16:21:20 +1300

    improvements - 1.1.0
    shortcode is now [shortcode name="" url=""]description here[/shortcode]

--- my-shortcode-plugin.php
+++ my-shortcode-plugin.php
@@ -2,32 +2,68 @@
 /**
  * Plugin Name: My Shortcode Plugin
  * Description: To display a custom message do: [my_shortcode_plugin name="name here"]
- * Version: 0.0.1
+ * Version: 1.1.0
  * Author: REInVent
  */
 
- function my_shortcode( $atts = array(), $content = null ) {
-   
+//register our shortcode
+add_shortcode('shortcode', 'my_shortcode');
+
+//register uninstall.php
+register_uninstall_hook(plugin_dir_url(__FILE__).'uninstall.php', 'uninstall_my_shortcode_plugin');
+
+function my_shortcode_stylesheet() {
+
+  wp_register_style('styles', plugin_dir_url(__FILE__).'styles.css');
+  
+  wp_enqueue_style('styles');
+  
+}
+
+function my_shortcode_init() {
+
+ function my_shortcode( $atts = array(), $content = null, $tag ) {
+
   //array of allowed html for tags wp_kses
-  $allow = array(
-    'a' => array(
-        'href' => array(),
-        'title' => array()
-    ),
-    'br' => array(),
-    'em' => array(),
-    'strong' => array(),
-);
+    $allow = array(
+      'br' => array(),
+      'em' => array(),
+      'strong' => array(),
+    );
 
-   //check if name has been set otherwise set the value to world
-   $atts_array = shortcode_atts( array( 'name' => 'World' ), $atts );
-   
-   //create a custom string to greet the user - don't trust the user
-   $text = "Hello " .wp_kses($atts_array['name'], $allow)."!"."<br>";
-   
-   //return our custom string
-   return $text;
- }
+    //check if name or url set otherwise assign them a default value
+    $atts_array = shortcode_atts( array( 
+      'name' => 'World',
+      'url' => '#'
+    ), $atts );
 
- //register our shortcode
- add_shortcode('my_shortcode_plugin', 'my_shortcode');
+    //create a custom string to greet the user - don't trust the user
+    $text = "<p class='text'>";
+    
+    $text .= "Hello ";
+
+    $text .= esc_attr($atts_array['name']);
+    
+    $text .= "!"; 
+
+    $text .= "<div><img src='";
+
+    $text .= esc_url($atts_array['url']);
+    
+    $text .= "' class='image' alt='my image'/></div><div class='text'>";
+
+    $text .= esc_attr($content);
+
+    $text.= "</div>";
+
+    $text .= "<br/>"; 
+
+    //return our custom string
+    return $text;
+  }
+  
+}
+
+
+add_action('init', 'my_shortcode_init');
+ ?>
---
+++ uninstall.php
@@ -0,0 +1,18 @@
+<?php
+
+//die when the file is called directly
+if(!defined('WP_UNINSTALL_PLUGIN')) {
+
+    die;
+
+}
+
+//not working yet/I haven't learned yet
+/* function uninstall_my_shortcode_plugin() {
+
+    $sql = "UPDATE wp_post SET post_content = REPLACE(post_content, '[shortcode]', '')";
+
+    $wpdb->query(sql);
+} */
+
+?>
